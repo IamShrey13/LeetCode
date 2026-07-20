76d1eb6925ad208bc90c4ade3ead4d3c6f62beed
Shrey <officialshrey13@gmail.com>
2026-07-21 05:01:32 +0530

    Python solution implementation

---
+++ Easy/14_Longest_Common_Prefix/Python_Solutions/14.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        longst = strs[0]
+        for item in strs[1:]:
+            i = 0
+            while i < len(longst) and i < len(item) and longst[i] == item[i]:
+                i +=1
+            longst = longst[:i]
+            if longst == "":
+                return ""
+        return longst
